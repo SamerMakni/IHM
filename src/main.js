a40bb034ccf0af05dad2a4762f415b92c6422708
Samer <dmc4samer@gmail.com>
2022-05-27 11:37:57 +0100

    First

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector("nav")
+const observer = new IntersectionObserver( 
+  ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
+  { threshold: [1] }
+);
+
+observer.observe(nav);
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0057bc',
+        secondary: '#bee0ff',
+        light: '#EEF4FF',
+      },
+    },
+  },
+  plugins: [],
+}
